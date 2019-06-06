7e61b435e754e890b255977266280c4dbdee61de
Ramón Blanco Caamaño <ramonblancocaamano@gmail.com>
2019-06-06 19:12:19 +0200

    0.02
    @NEW
     - Stable version.
    @TODO
     - Update user list.
     - message functionality.

---
+++ SERVER/src/java/entity/service/ApplicationConfig.java
@@ -0,0 +1,31 @@
+package entity.service;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ * @Authors: BLANCO CAAMANO, Ramon <ramonblancocaamano@gmail.com>
+ *           GREGORIO DURANTE, Nicola <ng.durante@gmail.com>
+ */
+@javax.ws.rs.ApplicationPath("rpc")
+public class ApplicationConfig extends Application {
+
+    @Override
+    public Set<Class<?>> getClasses() {
+        Set<Class<?>> resources = new java.util.HashSet<>();
+        addRestResourceClasses(resources);
+        return resources;
+    }
+
+    /**
+     * Do not modify addRestResourceClasses() method. It is automatically
+     * populated with all resources defined in the project. If required, comment
+     * out calling this method in getClasses().
+     */
+    private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(entity.service.MessageFacadeREST.class);
+        resources.add(entity.service.UserFacadeREST.class);
+        resources.add(entity.service.UserInfoFacadeREST.class);
+    }
+
+}
